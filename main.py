dac8f22aa0e63aac66a0d8e22809a887aafa04dd
eolandro <eolandro@gmx.com>
2018-08-27 00:58:16 -0500

    Error

---
+++ main.py
@@ -0,0 +1,126 @@
+# encoding: utf-8
+import kivy
+kivy.require('1.8.0')
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
+# hacer post y get a la api
+from kivy.network.urlrequest import UrlRequest
+from kivy.uix.screenmanager import Screen
+from kivy.metrics import dp
+
+class Acceso(App):
+	def reqError(self,req, Ret):
+		print(u'Error en la conexión al servidor')
+		print(Ret)
+		content=Label(text=u'Error en la conexión al servidor')
+		popup = Popup(
+			title='Mensaje', 
+			content=content,
+			size_hint=(1,1.0/3.0),
+		)
+		popup.open()
+		
+	def reqFail(self,req, Ret):
+		print(u'Error al realizar la petición')
+		print(Ret)
+		content=Label(text=u'Error al realizar la petición')
+		popup = Popup(
+			title='Mensaje', 
+			content=content,
+			size_hint=(1,1.0/3.0),
+		)
+		popup.open()
+	
+	def tokenListo(self,req,Ret):
+		print(u'+++ +++')
+		print(Ret)
+		if Ret['R'] == 200:
+			self.tknn = Ret['D']
+			req_body = '{"TKN":"'+Ret['D']+'"}'
+			
+			headers = {
+				'Content-Type':'application/json'
+			}
+			
+			print(self.baseurl +'/api/echo')
+			print(u'Body:',req_body)
+			req = UrlRequest(
+				url = self.baseurl +'/api/echo', 
+				on_success = self.envioCred,
+				on_error = self.reqError,
+				on_failure = self.reqFail,
+				decode = True,
+				req_body = req_body,
+				verify = False,
+				req_headers = headers
+			)
+			
+	def envioCred(self,req,Ret):
+		print(u'+++ +++')
+		print(u'Ret: ',Ret,req)
+		if Ret['R'] == 200:
+			content=Label(text=u'Done')
+			popup = Popup(
+				title='Alerta', 
+				content=content,
+				size_hint=(1,1.0/3.0)
+			)
+			popup.open()
+			
+	def salir(self,ins):
+		App.get_running_app().stop()
+		
+	def btnokPress(self,ins):
+		print(self.baseurl +'/api/getTokenM')
+		req = UrlRequest(
+			url = self.baseurl +'/api/getTokenM', 
+			on_success = self.tokenListo,
+			on_error = self.reqError,
+			on_failure = self.reqFail,
+			verify = False,
+			decode = True
+		)
+			
+		
+	def build(self,**kw):
+		self.baseurl = 'https://144.202.34.148'
+		self.tknn = ''
+		S = dp(10)
+		
+		layout = BoxLayout(
+			orientation='vertical',
+			padding = [S,S,S,S]
+		)
+		
+		btnok = Button(
+			text='Access',
+			font_size='20sp'
+		)
+		btnexit = Button(
+			text='Exit',
+			font_size='20sp'
+		)
+		
+		##########################
+		control = BoxLayout(orientation='horizontal')
+		control.add_widget(btnok)
+		control.add_widget(btnexit)
+		##########################
+		layout.add_widget(Widget())
+		layout.add_widget(control)
+		layout.add_widget(Widget())
+		
+		btnok.bind(on_press=self.btnokPress)
+		btnexit.bind(on_press=self.salir)
+		
+		
+		return layout
+
+if __name__ == '__main__':
+	M = Acceso()
+	M.run()
